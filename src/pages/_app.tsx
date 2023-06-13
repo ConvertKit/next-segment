8b3260511f39db16ec05817d63f700ef53f728db
Cory Miller <corydhmiller@gmail.com>
2023-06-13 14:50:11 +0200

    Set up initial segment tracking component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import SegmentTracking from "@/components/Segment/SegmentTracking"
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<SegmentTracking />
+			<Component {...pageProps} />
+		</>
+	)
 }
